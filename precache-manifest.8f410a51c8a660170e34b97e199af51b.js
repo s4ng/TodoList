88e33c0f3fb783a45d9cd2a60941b36a39960973
s4ng <zalcls951201@gmail.com>
2019-05-19 21:12:10 +0900

    Updates

---
+++ precache-manifest.8f410a51c8a660170e34b97e199af51b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8406f0c9dd076180ac4cf7a4aa91bb",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "1bc6dd020e43000e1935",
+    "url": "/TodoList/static/css/main.f3123b9a.chunk.css"
+  },
+  {
+    "revision": "bd0c9146313fb134b61e",
+    "url": "/TodoList/static/js/2.7c0a1338.chunk.js"
+  },
+  {
+    "revision": "1bc6dd020e43000e1935",
+    "url": "/TodoList/static/js/main.0d328b5b.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
